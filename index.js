1fe1f81283ae53ae613ee3d260eb33cd71e8e822
Avinash Gupta <73595465+tier3guy@users.noreply.github.com>
2021-08-21 00:31:03 +0530

    project finished

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const socket = io('http://localhost:8000');
+const socket = io('http://localhost:3000');
 
 const nameCont = document.getElementById('name');
 var user_name = '';
 
 const message_container = document.querySelector('.message-box');
 const sendBtn = document.getElementById('sendBtn');
+const screen = document.getElementById('chat_window');
 
 const joinRoom = () => {
   user_name = nameCont.value;
@@ -14,5 +15,40 @@ const joinRoom = () => {
   else{
     document.querySelector('.get-entry').style.display = "none";
     document.querySelector('.app').style.display = "block";
+    socket.emit('new-user-joined',user_name);
   }
 }
+
+const append = (message,position) => {
+  let messageElement = document.createElement('h6');
+  messageElement.innerText = message;
+  messageElement.classList.add(position);
+  if(position !== 'cenner') messageElement.classList.add('ms');
+  screen.append(messageElement);
+}
+
+socket.on('recieve', data => {
+  append(`${data.name} : ${data.message}`,'left');
+});
+
+socket.on('joining', user_name => {
+  append(`${user_name} has joined the chat`,'cenner');
+  console.log(user_name + 'connected');
+});
+
+sendBtn.addEventListener('click',() => {
+  var msg = message_container.value;
+  append(`You : ${msg}`,'right');
+  socket.emit('send-message',msg);
+  message_container.value = '';
+});
+
+sendBtn.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    var msg = message_container.value;
+    append(`You : ${msg}`,'right');
+    socket.emit('send-message',msg);
+    message_container.value = '';
+  }
+});
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const io = require('socket.io')(8000);
+const io = require('socket.io')(3000);
 const users = {};
 
 io.on('connection', (socket) => {
   console.log('connection build sucessfully');
   socket.on('new-user-joined', name => {
-    console.log(`${name} user connected`);
+    console.log(`${name} connected`);
     users[socket.id] = name;
-    socket.broadcast.emit('user-joined',name);
+    socket.broadcast.emit('joining',name);
   });
   socket.on('send-message', message => {
-    socket.broadcast.emit('recieve',{message : message, name : users[socket.id]})
+    socket.broadcast.emit('recieve',{message : message, name : users[socket.id]});
   })
   socket.on('disconnect', () => {
     console.log('user disconnected');
